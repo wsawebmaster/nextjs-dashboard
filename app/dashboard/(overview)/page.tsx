49ae65d16444f2351b8925c4d9bc2e0d59868140
Wagner Andrade <wsawebmaster@yahoo.com.br>
2026-06-28 18:50:32 -0300

    feat: carregamento streaming dentro do próprio componente e grupo de componentes

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,33 +1,28 @@
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "../../lib/data";
-import { Card } from "../../ui/dashboard/cards";
+import { Suspense } from "react";
+import CardWrapper, { Card } from "../../ui/dashboard/cards";
 import LatestInvoices from "../../ui/dashboard/latest-invoices";
 import RevenueChart from "../../ui/dashboard/revenue-chart";
+import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
-    const {
-        numberOfCustomers,
-        numberOfInvoices,
-        totalPaidInvoices,
-        totalPendingInvoices,
-    } = await fetchCardData();
-
-    const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
-
     return (
     <main>
         <h1 className="mb-4 text-xl md:text-2x1">
             Dashboard
         </h1>
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-            <Card title="Collected" value={totalPaidInvoices} type="collected" />
-            <Card title="Pending" value={totalPendingInvoices} type="pending" />
-            <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-            <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+            <Suspense fallback={<CardSkeleton />}>
+                <CardWrapper />
+            </Suspense>
         </div>
         <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-            <RevenueChart revenue={revenue} />
-            <LatestInvoices latestInvoices={latestInvoices} />
+            <Suspense fallback={<RevenueChartSkeleton />}>
+                <RevenueChart />
+            </Suspense>
+
+            <Suspense fallback={<LatestInvoicesSkeleton />}>
+                <LatestInvoices />
+            </Suspense>
         </div>
     </main>
 )}
